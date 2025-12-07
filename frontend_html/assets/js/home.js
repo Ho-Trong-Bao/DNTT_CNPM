7eb4a9616f7bad84509447744f104cfc4d26bbec
Master <hotrongbao27@gmail.com>
2025-12-07 13:39:50 +0700

    cập nhất kích thước ảnh bài đăng

--- frontend_html/assets/js/home.js
+++ frontend_html/assets/js/home.js
@@ -31,7 +31,7 @@ function renderBooksPage(books, page) {
         .map(book => `
             <div class="col-md-3">
                 <div class="card h-100 shadow-sm">
-                    <img src="${book.image}" class="card-img-top" style="height:200px; object-fit:cover;">
+                    <img src="${book.image}" class="card-img-top" style="height:400px; object-fit:cover;">
                     <div class="card-body">
                         <h5 class="card-title">${book.title}</h5>
                         <p class="text-muted">${book.author}</p>
