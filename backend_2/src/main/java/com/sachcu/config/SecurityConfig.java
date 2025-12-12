64d3a1395e32bcb09d381539af651110e56366b7
Master <hotrongbao27@gmail.com>
2025-12-12 19:45:12 +0700

    comiit

--- backend_2/src/main/java/com/sachcu/config/SecurityConfig.java
+++ backend_2/src/main/java/com/sachcu/config/SecurityConfig.java
@@ -92,6 +92,7 @@ public class SecurityConfig {
                 "http://localhost:3000",
                 "http://localhost:5173",
                 "http://127.0.0.1:5500",
+                "http://127.0.0.1:5501",
                 "https://your-frontend-domain.com"
         ));
 
